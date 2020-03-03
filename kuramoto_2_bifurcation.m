feb10e5a3c6e8d7dae5503fdc96aa8452fa102e2
jingenito <jingenito1530@gmail.com>
2020-03-03 10:16:50 -0500

    work done on the bifurcation diagram for the second order model

---
+++ kuramoto_2_bifurcation.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Create a bifurcation diagram of the 2nd Order Kuramoto Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear         % clear any variables
+clf           % clears any figures already up
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Parameters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n = 50; %number of oscillators
+w = randn(n,1); %Random internal frequencies chosen from normal distribution
+u_int = rand(n,1)*2*pi; %Random initial conditions
+u_prime_int = randn(n,1); %random initial velocity conditions
+
+%going to use the same connections for each (K,a) pair
+G = sw_graph(n,.2,.4);   %Adjacency matrix of network connections
+
+KVec = linspace(-50,50,100);
+a = 10;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Loop over all (K,a) pairs and track the long term behavior
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Z = []; %vector to track long term behavior
+for i=1:length(KVec)
+    
+    [t,u]=ode45(@(t,y) kuramoto_2(y,a,w,KVec(i),n,G),[0,500],[u_int; u_prime_int]);
+    Z(i) = u(length(u));
+end
+
+plot(KVec,Z)
