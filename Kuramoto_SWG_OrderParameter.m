6925e771e8624783b89e6cd48522166574d4d6f0
Malekith <jingenito@vcssoftware.com>
2020-03-31 23:16:12 -0400

    modified the order parmameter and implemented the l2-norm in the bifurcation diagram

--- Kuramoto_SWG_OrderParameter.m
+++ Kuramoto_SWG_OrderParameter.m
@@ -1,17 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Input a vector and an adjaceny matrix, and output the 
 %% modified complex order parameter.
-%%
-%% Using j as the compex number, sqrt(-1), to not be confused with the loop 
-%% counter i
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function val = Kuramoto_SWG_OrderParameter(x,W)
-z = 0; %will hold sum of all the complex nubmers
-for i=1:length(x)
-    for j=1:length(x)
-        z = z + W(i,j)*exp(1j*x(i)); 
-    end
-end
 
-z = z / length(x)^2; %take the average of the sum
-val = abs(z); %output the modulous
+val = abs(exp(1i.*x) * W);
--- kuramoto_2_bifurcation.m
+++ kuramoto_2_bifurcation.m
@@ -23,28 +23,32 @@ a = 3;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Loop over all (K,a) pairs and track the long term behavior
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Z = []; %vector to track long term behavior
+%storing result to calculate the l2 norm on each iteration
+h_sync = Kuramoto_SWG_OrderParameter(ones(1,n),G)'; %transposing once for l-2 norm
+
+Z = zeros(1,length(KVec)); %preallocating memory for optimization
 for i=1:length(KVec)
     [t,u]=ode45(@(t,y) kuramoto_2(y,a,w,KVec(i),n,G),[0,50],[u_int; u_prime_int]);
     
     r = u(length(t), 1:n); %get the theta vector 
-    Z(i) = Kuramoto_SWG_OrderParameter(r,G); %caclulate the complex order parameter
+    h = Kuramoto_SWG_OrderParameter(r,G); %caclulate the complex order parameter
+    Z(i) = h * h_sync;
     
     %set initial conditions to the previous solution
     u_int = r;
     u_prime_int = u(length(t), n+1:end);
 end
 
-Z1 = [];
+Z1 = zeros(1,length(KVec)); %preallocating memory for optimization
 for i=length(KVec):-1:1
     [t,u]=ode45(@(t,y) kuramoto_2(y,a,w,KVec(i),n,G),[0,50],[u_int; u_prime_int]);
     
     r = u(length(t), 1:n); %get the theta vector 
-    Z1(i) = Kuramoto_SWG_OrderParameter(r,G); %caclulate the complex order parameter
-
+    h = Kuramoto_SWG_OrderParameter(r,G); %caclulate the complex order parameter
+    Z1(i) = h * h_sync;
 end
 
-plot(KVec,Z,'.')
+plot(KVec,Z,'.','Color','b')
 hold on
-plot(KVec,Z1,'.')
+plot(KVec,Z1,'.','Color','r')
 hold off
