219ac1f9553f670221099c9b21e9d16e953b79c8
Malekith <jingenito@vcssoftware.com>
2020-04-09 22:03:01 -0400

    work done implementing the eigenvalue projections on both the negative and positive slope twisted states

--- Kuramoto_SWG_SynchOrderParameter.m
+++ Kuramoto_SWG_SynchOrderParameter.m
@@ -1,7 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Calculates h_inf order parameter for the synchronized
 % state of the Small World Graph model.
+%
+% Outputs a matrix where the first column consists of 
+% the positive eigenvalues and the second column 
+% consists of the negative eigenvalues.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function val = Kuramoto_SWG_SynchOrderParameter(u,beta,kappa,p,r)
+function val = Kuramoto_SWG_SynchOrderParameter(u,p,r)
 q = getMinEigenvalueIndex(p,r);
-val = sqrt(kappa / beta) * exp(1i * 2 * pi * q .* u);
+val(:,1) = exp(1i * 2 * pi * q .* u)';
+val(:,2) = exp(-1i * 2 * pi * q .* u)';
--- getMinEigenvalueIndex.m
+++ getMinEigenvalueIndex.m
@@ -6,8 +6,8 @@ function val = getMinEigenvalueIndex(p,r)
 min = 2*r + p - 4*r*p;
 val = 0;
 
-for k = 2:100
-    tmp = (pi * (k-1)^(-1)) * (1-2*p) * sin(2*pi*(k-1)*r);
+for k = 2:6
+    tmp = (pi * (k-1))^(-1) * (1-2*p) * sin(2*pi*(k-1)*r);
     if tmp < min 
         min = tmp;
         val = k - 1;
